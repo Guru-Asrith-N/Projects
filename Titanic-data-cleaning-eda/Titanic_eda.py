621db3329a0d2824efd0be2635e081fa3ffaae95
Guru-Asrith-N <147991595+Guru-Asrith-N@users.noreply.github.com>
2026-01-11 17:48:17 +0530

    Update Titanic_eda.py

--- Titanic-data-cleaning-eda/Titanic_eda.py
+++ Titanic-data-cleaning-eda/Titanic_eda.py
@@ -5,48 +5,49 @@ import numpy as np
 
 df = pd.read_csv("train.csv")
 
-# print(df.head())
-# print(df.info())
-# print(df.shape)
-#print(df.isnull().sum())
+print(df.head())
+print(df.info())
+print(df.shape)
+print(df.isnull().sum())
 
-# missing_percent = df.isnull().mean()*100
-# print(missing_percent)
+missing_percent = df.isnull().mean()*100
+print(missing_percent)
 
-# df.drop(columns = ['Cabin'],inplace = True)
-# df['Age'] = df['Age'].fillna(df['Age'].median())
-# df['Embarked']= df['Embarked'].fillna(df['Embarked'].mode()[0])
-# df.drop(columns=['PassengerId','Name','Ticket'],inplace=True)
+df.drop(columns = ['Cabin'],inplace = True)
+df['Age'] = df['Age'].fillna(df['Age'].median())
+df['Embarked']= df['Embarked'].fillna(df['Embarked'].mode()[0])
+df.drop(columns=['PassengerId','Name','Ticket'],inplace=True)
 
-# df['Sex'] = df['Sex'].map({'male':0, 'female':1})
-# df = pd.get_dummies(df, columns=['Embarked'], drop_first = True)
+df['Sex'] = df['Sex'].map({'male':0, 'female':1})
+df = pd.get_dummies(df, columns=['Embarked'], drop_first = True)
 
-# print(df.isnull().sum())
-# print(df.head())
+print(df.isnull().sum())
+print(df.head())
 
 
-#print(df['Survived'].value_counts(normalize=True))
+print(df['Survived'].value_counts(normalize=True))
 
-# sns.barplot(x='Sex',y = 'Survived',data = df)
-# plt.title("Survival Rate by gender")
-# plt.show()
+sns.barplot(x='Sex',y = 'Survived',data = df)
+plt.title("Survival Rate by gender")
+plt.show()
 
-# sns.barplot(x ='Pclass',y='Survived',data = df)
-# plt.title("Survival rate by class")
-# plt.show()
+sns.barplot(x ='Pclass',y='Survived',data = df)
+plt.title("Survival rate by class")
+plt.show()
 
-# sns.histplot(df['Age'],kde=True)
-# plt.title("Age distribution")
-# plt.show()
+sns.histplot(df['Age'],kde=True)
+plt.title("Age distribution")
+plt.show()
 
-# plt.figure(figsize=(10,6))
-# sns.heatmap(df.corr(), annot = True, cmap = 'coolwarm')
-# plt.title("Feature correlation")
-# plt.show()
+plt.figure(figsize=(10,6))
+sns.heatmap(df.corr(), annot = True, cmap = 'coolwarm')
+plt.title("Feature correlation")
+plt.show()
 
 numeric_df = df.select_dtypes(include=['int64','float64'])
 
 plt.figure(figsize=(10,6))
 sns.heatmap(numeric_df.corr(), annot  = True,cmap='coolwarm')
 plt.title("Correlation Heatmap(Numeric Features only)")
+
 plt.show()
